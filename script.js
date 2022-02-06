62b50e1d85a522e03db2b29836129cf6623cab18
ÂYaroslav Shishkin <company-rosstan@yandex.ru>
2022-02-06 23:31:51 +0300

    дlesson06 corrections 06.02

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 "use strict"
 
-let rondom = 55;
+const randomNum = function () {
 
+    let random = Math.floor(Math.random() * 100);
+    console.log(random)
 
+    return function questionGame() {
 
-function questionGame() {
+        let question = prompt("Угадай число от 1 до 100")
 
-    let question = prompt("Угадай число от 1 до 100")
+        if (question == random) {
+            return alert("Вы угадали!!!");
 
-    if (question == rondom){
-        return alert("Вы угадали!!!");
+        } else if (question == null) {
+            return alert("Игра окончена")
 
-    }else if (question == null ) {
-        return alert("Игра окончена")
+        } else if (question > random) {
+            alert("Загаданное число меньше")
 
-    }else if (question > rondom ) {
-        alert("Загаданное число меньше")
+        } else if (question < random) {
+            alert("Загаданное число больше")
 
-    }else if (question < rondom ) {
-        alert("Загаданное число больше")
+        } else if (question.replace(/\s/g, '').length === 0 || isNaN(question)) {
+            alert("Введи число!")
 
-    }else if (question.replace(/\s/g, '').length === 0 || isNaN(question)) {
-        alert("Введи число!")
-
+        }
+        questionGame()
     }
 
-questionGame();
 }
-questionGame();
+
+let step = randomNum()
+step()
